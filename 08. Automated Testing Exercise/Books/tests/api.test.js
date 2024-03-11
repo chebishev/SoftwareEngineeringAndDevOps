2eaca55d077c76a0bddc1f083d7651f8a6fb350d
chebishev <atanas.chebishev@gmail.com>
2024-03-11 12:31:56 +0200

    working cofiguration for API tests

--- 08. Automated Testing Exercise/Books/tests/api.test.js
+++ 08. Automated Testing Exercise/Books/tests/api.test.js
@@ -1,34 +1,29 @@
-import * as chai from "chai";
-import chaiHttp from "chai-http";
-const server = import("../../server");
-
-// const chai = import("chai");
-// const chaiHttp = import("chai-http");
-// const server = import("./server");
-// const expect = chai.expect;
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const server = require('../server');
+const expect = chai.expect;
 
 chai.use(chaiHttp);
 
-describe("Books API", () => {
-  let bookId;
-  it("should create a new book", (done) => {
-    const book = {
-      id: "1",
-      title: "The Great Gatsby",
-      author: "F. Scott Fitzgerald",
-    };
-    chai
-      .request(server)
-      .post("/books")
-      .send(book)
-      .end((err, res) => {
-        chai.expect(res).to.have.status(201);
-        chai.expect(res.body).to.be.a("object");
-        chai.expect(res.body).to.have.property("id");
-        chai.expect(res.body).to.have.property("title");
-        chai.expect(res.body).to.have.property("author");
-        bookId - res.body.id;
-        done();
-      });
-  });
+describe('Books API', () => {
+    let bookId;
+
+    it('should POST a book', async () => {
+        const book = { id: "1", title: "Test Book", author: "Test Author" };
+        const res = await chai.request(server).post('/books').send(book);
+        
+        expect(res).to.have.status(201);
+        expect(res.body).to.be.a('object');
+        expect(res.body).to.have.property('id');
+        expect(res.body).to.have.property('title');
+        expect(res.body).to.have.property('author');
+        bookId = res.body.id;
+    });
+
+    it('should GET all books', async () => {
+        const res = await chai.request(server).get('/books');
+        
+        expect(res).to.have.status(200);
+        expect(res.body).to.be.a('array');
+    });
 });
